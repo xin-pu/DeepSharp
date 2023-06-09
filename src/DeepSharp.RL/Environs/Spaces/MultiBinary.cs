320895d767081dea44e7c88c9dcf06d4b79136cf
xin-pu <pu.xin@outlook.com>
2023-06-09 16:21:30 +0800

    Add more Space here but code later.

---
+++ src/DeepSharp.RL/Environs/Spaces/MultiBinary.cs
@@ -0,0 +1,13 @@
+﻿namespace DeepSharp.RL.Environs.Spaces
+{
+    /// <summary>
+    ///     Todo
+    /// </summary>
+    public class MultiBinary : MultiDisperse
+    {
+        public MultiBinary(torch.Tensor low, torch.Tensor high, long[] shape, torch.ScalarType type,
+            DeviceType deviceType = DeviceType.CUDA, long seed = 1) : base(low, high, shape, type, deviceType, seed)
+        {
+        }
+    }
+}
---
+++ src/DeepSharp.RL/Environs/Spaces/MultiDisperse.cs
@@ -0,0 +1,23 @@
+﻿namespace DeepSharp.RL.Environs.Spaces
+{
+    /// <summary>
+    ///     Todo
+    /// </summary>
+    public class MultiDisperse : DigitalSpace
+    {
+        public MultiDisperse(torch.Tensor low, torch.Tensor high, long[] shape, torch.ScalarType type,
+            DeviceType deviceType = DeviceType.CUDA, long seed = 1) : base(low, high, shape, type, deviceType, seed)
+        {
+        }
+
+        public override torch.Tensor Sample()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void CheckType()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
