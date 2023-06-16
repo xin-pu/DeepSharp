34e4bbf8756095a2c688db9a073bb7805fc1cfb9
xin-pu <pu.xin@outlook.com>
2023-06-16 15:03:37 +0800

    Add EnvironWarpper

---
+++ src/DeepSharp.RL/Environs/Wappers/EnvironWarpper.cs
@@ -0,0 +1,16 @@
+﻿namespace DeepSharp.RL.Environs.Wappers
+{
+    public abstract class EnvironWarpper
+    {
+        protected EnvironWarpper(Environ<Space, Space> environ)
+        {
+            Environ = environ;
+        }
+
+        public Environ<Space, Space> Environ { set; get; }
+
+        public abstract Step Step(Act act, int epoch);
+
+        public abstract Observation Reset();
+    }
+}
---
+++ src/DeepSharp.RL/Environs/Wappers/MaxAndSkipEnv.cs
@@ -0,0 +1,47 @@
+﻿namespace DeepSharp.RL.Environs.Wappers
+{
+    public abstract class MaxAndSkipEnv : EnvironWarpper
+    {
+        public int Skip { protected set; get; }
+        public Queue<Observation> Observations { protected set; get; }
+
+        protected MaxAndSkipEnv(Environ<Space, Space> environ, int skip)
+            : base(environ)
+        {
+            Skip = skip;
+            Observations = new Queue<Observation>(2);
+        }
+
+
+        public override Step Step(Act act, int epoch)
+        {
+            var totalReward = 0f;
+            var isComplete = false;
+            var oldobs = Environ.Observation!;
+            foreach (var _ in Enumerable.Range(0, Skip))
+            {
+                var step = Environ.Step(act, epoch);
+                Observations.Enqueue(step.StateNew);
+                totalReward += step.Reward.Value;
+                if (step.IsComplete)
+                {
+                    isComplete = true;
+                    break;
+                }
+            }
+
+            var obs = Observations.Select(a => a.Value!).ToList();
+            var max = new Observation(torch.max(torch.vstack(obs)));
+            var reward = new Reward(totalReward);
+            return new Step(oldobs, act, max, reward, isComplete);
+        }
+
+        public override Observation Reset()
+        {
+            Observations.Clear();
+            var obs = Environ.Reset();
+            Observations.Enqueue(obs);
+            return obs;
+        }
+    }
+}
