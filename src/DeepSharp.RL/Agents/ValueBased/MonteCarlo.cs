e06e95189a50972f9f4bc1fa9ac0730c3784b0d9
xin-pu <pu.xin@outlook.com>
2023-06-29 16:45:51 +0800

    Update ...

--- src/DeepSharp.RL/Agents/ValueBased/MonteCarlo.cs
+++ src/DeepSharp.RL/Agents/ValueBased/MonteCarlo.cs
@@ -23,24 +23,13 @@ namespace DeepSharp.RL.Agents
 
         public Episode[] Learn()
         {
-            Environ.Reset();
-            var episode = new Episode();
-            var epoch = 0;
-            var act = GetEpsilonAct(Environ.Observation!.Value!);
-            while (Environ.IsComplete(epoch) == false)
-            {
-                epoch++;
-                var step = Environ.Step(act, epoch);
-                act = Update(step);
-                episode.Steps.Add(step);
-                Environ.CallBack?.Invoke(step);
-                Environ.Observation = step.StateNew; /// It's import for Update Observation
-            }
+            var episodes = Enumerable.Range(0, N)
+                .Select(i => RunEpisode())
+                .ToList();
 
-            var orginalReward = episode.Steps.Sum(a => a.Reward.Value);
-            var sumReward = orginalReward;
-            episode.SumReward = new Reward(sumReward);
-            return episode;
+            // Todo Update Value Table Here 
+
+            return episodes.ToArray();
         }
     }
 }
