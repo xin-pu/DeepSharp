ec8ab92fd88dc8abf1f49f925090451296c1b4af
xin-pu <pu.xin@outlook.com>
2023-06-30 12:27:31 +0800

    Code DQN

--- src/DeepSharp.RL/Agents/ValueBased/DQN.cs
+++ src/DeepSharp.RL/Agents/ValueBased/DQN.cs
@@ -1,4 +1,5 @@
 ﻿using DeepSharp.RL.Environs;
+using static TorchSharp.torch.optim;
 
 namespace DeepSharp.RL.Agents
 {
@@ -9,18 +10,28 @@ namespace DeepSharp.RL.Agents
     /// </summary>
     public class DQN : Agent
     {
-        public DQN(Environ<Space, Space> env, int c, int n)
+        /// <summary>
+        /// </summary>
+        /// <param name="env"></param>
+        /// <param name="c">update interval</param>
+        /// <param name="n">Capacity of Experience pool</param>
+        public DQN(Environ<Space, Space> env, int c, int n, float gamma = 0.9f)
             : base(env, "DQN")
         {
             ObservationSpace = (int) env.ObservationSpace!.N;
             ActionSpace = (int) env.ActionSpace!.N;
             C = c;
             N = n;
+            Gamma = gamma;
             Net = new Net(ObservationSpace, 128, ActionSpace, DeviceType.CPU);
+            Optimizer = Adam(Net.parameters(), 0.01);
+            Loss = CrossEntropyLoss();
+            Experience = new ExperienceReplayBuffer(N);
         }
 
         public int ActionSpace { protected set; get; }
         public int ObservationSpace { protected set; get; }
+        public float Gamma { protected set; get; }
 
         /// <summary>
         ///     update interval
@@ -32,16 +43,63 @@ namespace DeepSharp.RL.Agents
         /// </summary>
         public int N { protected set; get; }
 
-        public Net Net { set; get; }
-
+        public Net Net { protected set; get; }
+        public Optimizer Optimizer { protected set; get; }
+        public Loss<torch.Tensor, torch.Tensor, torch.Tensor> Loss { protected set; get; }
+        public ExperienceReplayBuffer Experience { protected set; get; }
 
         public override Act GetPolicyAct(torch.Tensor state)
         {
-            var actionProb = Net.forward(state);
-            var maxActIndex = torch.argmax(actionProb).ToInt32();
-            var actTensor = torch.from_array(new[] {maxActIndex});
-            return new Act(actTensor);
+            return GetPredValues(state).Item1;
         }
+
+
+        public Tuple<Act, torch.Tensor> GetPredValues(torch.Tensor state)
+        {
+            var values = Net.forward(state);
+            var bestActIndex = torch.argmax(values).ToInt32();
+            var actTensor = torch.from_array(new[] {bestActIndex});
+            var act = new Act(actTensor);
+            return new Tuple<Act, torch.Tensor>(act, values);
+        }
+
+        public Episode Learn()
+        {
+            Environ.Reset();
+            var episode = new Episode();
+            var epoch = 0;
+            while (Environ.IsComplete(epoch) == false)
+            {
+                epoch++;
+                var (act, values) = GetPredValues(Environ.Observation!.Value!);
+                var actIndex = act.Value!.ToInt32();
+                var step = Environ.Step(act, epoch);
+
+                Experience.Append(step);
+
+                var expSample = Experience.Sample();
+                var argMaxValue = GetPredValues(expSample.StateNew.Value!).Item2.data<float>().Max();
+                var rr = expSample.Reward.Value;
+                var y_true = expSample.IsComplete
+                    ? rr
+                    : rr + Gamma * argMaxValue;
+                values[actIndex] = y_true;
+                var r_pred = Net.forward(expSample.State.Value!);
+                var output = Loss.call(r_pred, values);
+                Optimizer.zero_grad();
+                output.backward();
+
+                episode.Steps.Add(step);
+                Environ.CallBack?.Invoke(step);
+                Environ.Observation = step.StateNew; /// It's import for Update Observation
+            }
+
+            var orginalReward = episode.Steps.Sum(a => a.Reward.Value);
+            var sumReward = orginalReward;
+            episode.SumReward = new Reward(sumReward);
+            return episode;
+        }
+
 
         public override Episode RunEpisode(PlayMode playMode = PlayMode.Agent)
         {
@@ -59,7 +117,6 @@ namespace DeepSharp.RL.Agents
                     _ => throw new ArgumentOutOfRangeException(nameof(playMode), playMode, null)
                 };
                 var step = Environ.Step(act, epoch);
-                //Update(step);
                 episode.Steps.Add(step);
                 Environ.CallBack?.Invoke(step);
                 Environ.Observation = step.StateNew; /// It's import for Update Observation
@@ -70,5 +127,43 @@ namespace DeepSharp.RL.Agents
             episode.SumReward = new Reward(sumReward);
             return episode;
         }
+    }
+
+
+    public class ExperienceReplayBuffer
+    {
+        public ExperienceReplayBuffer(int N)
+        {
+            Capacity = N;
+            Buffers = new Queue<Step>(N);
+        }
+
+        public int Capacity { protected set; get; }
+
+        public void Append(Step step)
+        {
+            if (Buffers.Count == Capacity) Buffers.Dequeue();
+            Buffers.Enqueue(step);
+        }
+
+        public void Append(IEnumerable<Step> steps)
+        {
+            steps.ToList().ForEach(Append);
+        }
+
+        public void Append(Episode episode)
+        {
+            Append(episode.Steps);
+        }
+
+        public Step Sample()
+        {
+            var length = Buffers.Count;
+            var probs = torch.from_array(Enumerable.Repeat(1, length).ToArray(), torch.ScalarType.Float32);
+            var randomIndex = torch.multinomial(probs, 1L).ToInt32();
+            return Buffers.ElementAt(randomIndex);
+        }
+
+        public Queue<Step> Buffers { set; get; }
     }
 }
--- src/TorchSharpTest/RLTest/DQNTest.cs
+++ src/TorchSharpTest/RLTest/DQNTest.cs
@@ -41,5 +41,40 @@ namespace TorchSharpTest.RLTest
             var act = dqn.GetPolicyAct(frozenLake.Observation!.Value!);
             Print(act);
         }
+
+
+        [Fact]
+        public void KArmedBanditMain()
+        {
+            /// Step 1 Create a 4-Armed Bandit
+            var kArmedBandit = new KArmedBandit(new[] {0.4, 0.80, 0.72, 0.70}) {Gamma = 0.95f};
+
+            /// Step 2 Create AgentCrossEntropy with 0.7f percentElite as default
+            var agent = new DQN(kArmedBandit, 1, 100, 0.1f);
+            Print(kArmedBandit);
+
+            var i = 0;
+            var testEpisode = 20;
+            var bestReward = 0f;
+            while (true)
+            {
+                i++;
+                kArmedBandit.Reset();
+                agent.Learn();
+
+
+                var episode = agent.RunEpisodes(testEpisode);
+                var reward = episode.Average(a => a.SumReward.Value);
+
+                bestReward = new[] {bestReward, reward}.Max();
+                Print($"{agent} Play:{i:D3}\t {reward}");
+                if (bestReward > 16.2)
+                    break;
+            }
+
+            var e = agent.RunEpisode();
+            var act = e.Steps.Select(a => a.Action);
+            Print(string.Join("\r\n", act));
+        }
     }
 }
