a30f2c99d470f418240fe4a840a877dc8172c447
Xin.Pu <pu.xin@outlook.com>
2023-07-06 15:02:40 +0800

    add reinforce learning method (NN). Policy Based funcion.#4

---
+++ src/DeepSharp.RL/Agents/PolicyBased/Reinforce.cs
@@ -0,0 +1,76 @@
+﻿using DeepSharp.RL.Environs;
+using static TorchSharp.torch.optim;
+
+namespace DeepSharp.RL.Agents
+{
+    public class Reinforce : PolicyGradientAgengt
+    {
+        public Reinforce(Environ<Space, Space> env,
+            int batchsize = 4,
+            float gamma = 0.99f,
+            float alpha = 0.01f)
+            : base(env, "Reinforce")
+        {
+            Batchsize = batchsize;
+            Gamma = gamma;
+            Alpha = alpha;
+
+            Optimizer = Adam(PolicyNet.parameters(), Alpha);
+        }
+
+        /// <summary>
+        ///     Episodes send to train
+        /// </summary>
+        public int Batchsize { protected set; get; }
+        public float Gamma { protected set; get; }
+        public float Alpha { protected set; get; }
+
+
+        public Optimizer Optimizer { protected set; get; }
+
+
+        public override LearnOutcome Learn()
+        {
+            var learnOutCome = new LearnOutcome();
+
+            var episode = RunEpisode();
+            var steps = episode.Steps;
+            learnOutCome.AppendStep(episode.Steps);
+
+            steps.Reverse();
+            Optimizer.zero_grad();
+
+            var g = 0f;
+
+            foreach (var s in steps)
+            {
+                var reward = s.Reward.Value;
+                var state = s.PreState.Value!.unsqueeze(0);
+                var action = s.Action.Value!.view(-1, 1).to(torch.ScalarType.Int64);
+                var logProb = torch.log(PolicyNet.forward(state)).gather(1, action);
+
+                g = Gamma * g + reward;
+
+                var loss = -logProb * g;
+                loss.backward();
+            }
+
+            Optimizer.step();
+
+            return learnOutCome;
+        }
+
+        
+
+        public override void Save(string path)
+        {
+            if (File.Exists(path)) File.Delete(path);
+            PolicyNet.save(path);
+        }
+
+        public override void Load(string path)
+        {
+            PolicyNet.load(path);
+        }
+    }
+}
---
+++ src/TorchSharpTest/RLTest/PolicyBasedTest/ReinforceTest.cs
@@ -0,0 +1,33 @@
+﻿using DeepSharp.RL.Agents;
+using DeepSharp.RL.Environs;
+using DeepSharp.RL.Trainers;
+
+namespace TorchSharpTest.RLTest.PolicyBasedTest
+{
+    public class ReinforceTest:AbstractTest
+    {
+        public ReinforceTest(ITestOutputHelper testOutputHelper) 
+            : base(testOutputHelper)
+        {
+        }
+
+        [Fact]
+        public void TestReinforce()
+        {
+            var kArmedBandit = new KArmedBandit(new[] {0.4, 0.85, 0.75, 0.75}) {Gamma = 0.95f};
+            var agent = new Reinforce(kArmedBandit);
+            var trainer = new RLTrainer(agent, Print);
+            trainer.Train(0.9f, 500, "", 20, 2);
+        }
+
+        [Fact]
+        public void VeirfyReinforce()
+        {
+            var kArmedBandit = new KArmedBandit(new[] {0.4, 0.85, 0.75, 0.75}) {Gamma = 0.95f};
+            var agent = new Reinforce(kArmedBandit);
+            agent.Load("[Agent[Reinforce]]_230_0.90.st");
+            var episode = agent.RunEpisode();
+            Print(episode);
+        }
+    }
+}
